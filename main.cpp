5d00c4d0de7886f6ef47638ebf96d5e57be4b128
COLNAY--DELPHIN Eve <Eve.Colnay--Delphin@bordeaux-inp.fr>
2024-10-16 11:28:49 +0200

    pilote de la fréquence

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+#define BLINKING_RATE     100ms
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
@@ -16,6 +16,7 @@ InterruptIn button(BUTTON1);
 using namespace std::chrono;
 Timer t;
 Ticker flipper;
+int cpt = 0, counter = 2;
 
 void flip_on()
 {
@@ -29,32 +30,30 @@ void flip_off()
 
 void flip()
 {
-    led2 = !led2;
+    cpt = cpt + 1;
+    if (cpt == counter)
+    {
+        led2 = !led2; //toggle la led si le compteur et la fréquence sont les mêmes.
+        cpt = 0; //remise à zéro du compteur. 
+    }
 }
 
+void change_freq()
+{
+    counter = counter + 1; //diminution de la fréquence de clignottement.  
+}
 
 int main()
 {
-    button.rise(&flip_on);
-    button.fall(&flip_off);
-    flipper.attach(&flip, 2.0);
+    button.rise(&change_freq);
+    //button.fall(&flip_off);
+    flipper.attach(&flip, 100ms);
 
     while (true) 
     {
-        //printf("Test 1, 2. Test\n");
-        //printf("Etat du bouton : %d \n", button.read());
-
         led1 = !led1;
-        // if (button == 1)
-        // {
-        //     t.start();
-        // }
-        // else
-        // {
-        //     t.stop();
-        //     printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
-        //     t.reset();
-        // }
+        printf("Temps de fréquence de la led 2 : %d x plus que la led 1.\n", counter);
+
 
         ThisThread::sleep_for(BLINKING_RATE);
     }
