185b930643f8a30fc28ee526e6bc67d2f1db1c99
COLNAY--DELPHIN Eve <Eve.Colnay--Delphin@bordeaux-inp.fr>
2024-10-16 10:42:40 +0200

    ticker

--- main.cpp
+++ main.cpp
@@ -8,21 +8,28 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-DigitalOut led(LED1);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
 
 InterruptIn button(BUTTON1);
 
 using namespace std::chrono;
 Timer t;
+Ticker flipper;
 
 void flip_on()
 {
-    led = 1;
+    led1 = 1;
 }
 
 void flip_off()
 {
-    led = 0;
+    led1 = 0;
+}
+
+void flip()
+{
+    led2 = !led2;
 }
 
 
@@ -30,23 +37,24 @@ int main()
 {
     button.rise(&flip_on);
     button.fall(&flip_off);
+    flipper.attach(&flip, 2.0);
 
     while (true) 
     {
-        printf("Test 1, 2. Test\n");
-        printf("Etat du bouton : %d \n", button.read());
+        //printf("Test 1, 2. Test\n");
+        //printf("Etat du bouton : %d \n", button.read());
 
-        //led = button;
-        if (button == 1)
-        {
-            t.start();
-        }
-        else
-        {
-            t.stop();
-            printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
-            t.reset();
-        }
+        led1 = !led1;
+        // if (button == 1)
+        // {
+        //     t.start();
+        // }
+        // else
+        // {
+        //     t.stop();
+        //     printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+        //     t.reset();
+        // }
 
         ThisThread::sleep_for(BLINKING_RATE);
     }
