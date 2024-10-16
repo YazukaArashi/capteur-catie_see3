73529bd42b0f1efe1c0f38f28cfac671648cd05a
COLNAY--DELPHIN Eve <Eve.Colnay--Delphin@bordeaux-inp.fr>
2024-10-16 10:30:52 +0200

    interruption

--- main.cpp
+++ main.cpp
@@ -15,31 +15,40 @@ InterruptIn button(BUTTON1);
 using namespace std::chrono;
 Timer t;
 
-void flip()
+void flip_on()
 {
-    led = !led;
+    led = 1;
+}
+
+void flip_off()
+{
+    led = 0;
 }
 
 
 int main()
 {
-button.rise(&flip);
+    button.rise(&flip_on);
+    button.fall(&flip_off);
 
     while (true) 
     {
         printf("Test 1, 2. Test\n");
         printf("Etat du bouton : %d \n", button.read());
 
-        // led = button;
-        
-        t.start();
-        printf("Hello World!\n");
-        t.stop();
-        printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+        //led = button;
+        // if (button == 1)
+        // {
+        //     t.start();
+        // }
+        // else
+        // {
+        //     t.stop();
+        //     printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+        //     t.reset();
+        // }
 
         ThisThread::sleep_for(BLINKING_RATE);
     }
-
-
     
 }
